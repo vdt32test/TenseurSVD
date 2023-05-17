4aa658d017638c515a2af2d5d628ff233833db58
mrmac <mrmac@mrmacs-MacBook-Air.local>
2023-05-17 14:58:41 +0200

    fixed tabulation

--- utils.hpp
+++ utils.hpp
@@ -6,16 +6,16 @@ const char WHITE[] = "\033[1;37m";
 const char NORM[] = "\033[0m";
 const float EPSILON = 0.000001;
 
-#define TEST_ASSERT(what,op,ref)                                \
-{									                            \
-	auto result = (what);						                \
-	auto comp = (ref);						                    \
-									                            \
-	if(!(result op comp))                                       \
-    {                                                           \
-        std::cout << RED << "Assertion failed:" << std::endl;   \
-		std::cout << #what << " = " << result << std::endl;	    \
+#define TEST_ASSERT(what,op,ref)\
+{\
+	auto result = (what);\
+	auto comp = (ref);\
+	\
+	if(!(result op comp))\
+    {\
+        std::cout << RED << "Assertion failed:" << std::endl;\
+		std::cout << #what << " = " << result << std::endl;\
 		std::cout << #ref << " = " << comp << NORM << std::endl;\
-        assert(what op ref);                                    \
-    }                                                           \
+        assert(what op ref);\
+    }\
 }
