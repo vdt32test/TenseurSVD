f9daca014870e0c2fdaf3f0e47ef6a93f1ec7105
mrmac <mrmac@mrmacs-MacBook-Air.local>
2023-05-17 14:53:56 +0200

    added utils

---
+++ utils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+const char GREEN[] = "\033[1;32m";
+const char RED[] = "\033[1;31m";
+const char WHITE[] = "\033[1;37m";
+const char NORM[] = "\033[0m";
+const float EPSILON = 0.000001;
+
+#define TEST_ASSERT(what,op,ref)                                \
+{									                            \
+	auto result = (what);						                \
+	auto comp = (ref);						                    \
+									                            \
+	if(!(result op comp))                                       \
+    {                                                           \
+        std::cout << RED << "Assertion failed:" << std::endl;   \
+		std::cout << #what << " = " << result << std::endl;	    \
+		std::cout << #ref << " = " << comp << NORM << std::endl;\
+        assert(what op ref);                                    \
+    }                                                           \
+}
